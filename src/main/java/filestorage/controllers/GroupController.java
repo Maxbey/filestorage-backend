581042e41492f94c024f64c237d565377dec4179
Maxbey <maximbeiner@gmail.com>
2017-12-03 20:27:51 +0600

    Add CORS support

--- src/main/java/filestorage/controllers/GroupController.java
+++ src/main/java/filestorage/controllers/GroupController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Set;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/group")
 public class GroupController extends AbstractController{
--- src/main/java/filestorage/controllers/UserController.java
+++ src/main/java/filestorage/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import filestorage.models.User;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController extends AbstractController{
